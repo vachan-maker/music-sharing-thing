3e50e108142b7b3173b37ebd6a55334cdd9235f9
vachan-maker <vachan@tutanota.com>
2024-09-13 19:21:31 +0530

    remove form component. Trying to fix why my form won't submit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,24 @@
 	export let data;
 </script>
 
-<Form />
+<main class="container form-container">
+	<form method="POST">
+		<label for="name-of-song">Enter name of song(label)</label>
+		<input type="text" name="name-of-song" id="name-of-song" />
+		<label for="url-spotify">Enter Spotify URL</label>
+		<input type="url" id="url-spotify" name="url-spotify" required />
+		<label for="description">Write something about the song</label>
+		<input
+			type="text"
+			id="description"
+			placeholder="Tell us why you love/hate the song..hehe"
+			name="description"
+			required
+		/>
+		<button>Submit</button>
+	</form>
+</main>
+
 <section class="container">
 	{#each data.music as m}
 		<article>
@@ -18,3 +35,9 @@
 		</article>
 	{/each}
 </section>
+
+<style>
+	.form-container {
+		padding-bottom: 2rem;
+	}
+</style>
