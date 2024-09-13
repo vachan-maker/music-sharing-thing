568eb1120b39362230216f4fd5d77a8d1dfa585b
vachan-maker <vachan@tutanota.com>
2024-09-13 21:55:59 +0530

    source code url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <header>
 	<div class="container">
 		<h1>Share the Music you Love</h1>
+		<p><a href="https://github.com/vachan-maker/music-sharing-thing">Source Code</p>
 	</div>
 </header>
 <div class="container">
