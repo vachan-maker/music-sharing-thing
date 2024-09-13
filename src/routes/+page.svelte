d16900d1a2a09e7482344fe7a59a7bdb46ff0fad
vachan-maker <vachan@tutanota.com>
2024-09-13 22:08:02 +0530

    remove label text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <main class="container form-container">
 	<form method="POST">
-		<label for="name-of-song">Enter name of song(label)</label>
+		<label for="name-of-song">Enter name of song</label>
 		<input type="text" name="name-of-song" id="name-of-song" />
 		<label for="url-spotify">Enter <strong>Spotify</strong> URL</label>
 		<input type="url" id="url-spotify" name="url-spotify" pattern=".*spotify.*"required />
